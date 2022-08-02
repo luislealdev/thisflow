102b75161a8afbd00317921e401a9671b3134e4f
Luis Leal <luisrrleal@gmail.com>
2022-08-01 23:08:44 -0500

    Add background and start creating the others parts

--- src/home/ThisFlowHome.jsx
+++ src/home/ThisFlowHome.jsx
@@ -1,16 +1,32 @@
 import React from "react";
-import { Donations, FloatButton, ImageGallery, NavBar, InitialInfo } from "./components";
+import {
+  Donations,
+  FloatButton,
+  ImageGallery,
+  NavBar,
+  InitialInfo,
+} from "./components";
 import MediaControlCard from "./components/MediaControlCard";
+import "../home/components/styles.css";
 
 export const ThisFlowHome = () => {
   return (
     <>
       <NavBar />
-      <InitialInfo/>
+      <InitialInfo />
+      <div className="bg">
+      <h5>Create your <span>own gallery</span></h5>
       <ImageGallery />
-      <MediaControlCard/>
+      <h5>Let your fans play your music from the website </h5>
+      <MediaControlCard />
+      <h5>Add yout social media</h5>
+
+      <h6>Help us to be better!</h6>
+      <Donations />
+      </div>
+
       <FloatButton />
-      <Donations/>
+
     </>
   );
 };
