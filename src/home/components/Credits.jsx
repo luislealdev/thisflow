7bf5ba12cbbcd9c9b0c95bb0b2b6aa0d13748ea6
Luis Leal <luisrrleal@gmail.com>
2022-08-07 11:15:58 -0500

    Add info to the credits page

--- src/home/components/Credits.jsx
+++ src/home/components/Credits.jsx
@@ -1,16 +1,31 @@
-
 export const Credits = () => {
   return (
     <div className="credits">
       <div>
         <h3>About</h3>
-        <p></p>
+        <p>
+          Create a website for an artist who's starting could be expensive,
+          thats why <span>thisflow</span>...
+        </p>
       </div>
       <div>
         <h3>Donations</h3>
+        <p>
+          <span>Thisflow</span> is totally free and will be totally free
+          forever, but there are ...
+        </p>
       </div>
       <div>
         <h3>Follow</h3>
+        <p>
+          Hey! Im Luis Leal and i created <span>thisflow</span>, you can follow
+          me on my social media to see new apps. <br />
+          Of course i have my personal page on <span>thisflow</span> to check
+          it, <a>click here!</a>
+        </p>
+      </div>
+      <div>
+        <h5>Copyright thisflow 2022. All rights reserved</h5>
       </div>
     </div>
   );
