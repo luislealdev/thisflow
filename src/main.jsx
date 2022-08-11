71e0398b0ea6c90b6048d6e291f383948b15d892
Luis Leal <luisrrleal@gmail.com>
2022-08-11 15:01:35 -0500

    Change BrowserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { store } from './store/store'
 import './styles.css'
 import { ThisFlowApp } from './ThisFlowApp'
@@ -9,9 +9,9 @@ import { ThisFlowApp } from './ThisFlowApp'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter> {/*Change BrowserRouter to HashRouter */}
         <ThisFlowApp />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 )
